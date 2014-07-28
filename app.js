55264d73abb4f7329432bb123f00c93ec490fa17
MaRisa Jackson <mhljackson@gmail.com>
2014-07-27 19:33:15 -0500

    refactored with express

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var http = require('http');
-var connect = require('connect');
 var directory = 'public';
 var port = process.env.PORT;
+var less = require('express-less');
 
-var app = connect()
-  .use(connect.logger(':remote-addr -> :method :url [:status]'))
-  .use(connect.static(directory));
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+      res.sendfile('./public/index.html');
+});
+app.use(express.static(__dirname + '/public'));
+app.use('/less', less(__dirname + '/less'));
 
 http.createServer(app).listen(port, function(){
   console.log('Node server listening. Port: ' + port + ', Directory: ' + directory);
